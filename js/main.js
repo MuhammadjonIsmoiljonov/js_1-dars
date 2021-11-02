a9ec3959304d7672f28ba83ae94f080d075e53cf
MuhammadjonIsmoiljonov <muhammadjonismoiljonov0@gmail.com>
2021-11-03 00:22:11 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+prompt("Yashash viloyatingiz:");
+prompt("yashash tumaningiz:");
+prompt("Ismingizni kiriting:");
+prompt("Familyangizni kiriting:");
+prompt("Otangizni ismi:");
+prompt("yoshingiz:");
+prompt("Uy raqamingiz:");
+prompt("Telfon raqamingiz:");
+prompt("Ishlash joyingiz:");
+prompt("Qiziqishlaringizni yozing:");
+
+
+var malumot = "Yashash viloyatingiz:Toshkent" + "\n" + "Yashash tumanungiz:Chochtepa" + "\n" + "Ismingizni kiriting:Muhammadjon" + "\n" + "  Familyangizni kiriting:Ismoiljonov" + "\n" + "Otangizning ismi:Erkinjon" + "\n" + "Qiziqishlaringiz:biznes va IT" + "\n" + "yoshingiz:19" + "\n" + "Uy raqamingiz:9" + "\n" + "Telefon raqamingiz:998913586057" + "\n" + "Ishlash joyingiz:Google";
+alert("Yashash viloyatingiz:Toshkent" + "\n" + "Yashash tumanungiz:Chochtepa" + "\n" + "Ismingizni kiriting:Muhammadjon" + "\n" + "  Familyangizni kiriting:Ismoiljonov" + "\n" + "Otangizning ismi:Erkinjon" + "\n" + "Qiziqishlaringiz:biznes va IT" + "\n" + "yoshingiz:19" + "\n" + "Uy raqamingiz:9" + "\n" + "Telefon raqamingiz:998913586057" + "\n" + "Ishlash joyingiz:Google");
+console.log(malumot);
